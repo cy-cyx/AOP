0c33217bb8f1b7cceacafa1ec123d1f72aabd3f3
caiyixiang <caiyixiang@joyy.com>
2022-07-06 19:24:52 +0800

    尝试使用ASM在方法字节码后面插入代码

--- buildSrc/src/main/java/com/plugin/MainActivityOnCreateAdapter.java
+++ buildSrc/src/main/java/com/plugin/MainActivityOnCreateAdapter.java
@@ -1,6 +1,7 @@
 package com.plugin;
 
 import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
 
 /**
  * Time:2022/6/14 16:06
@@ -18,7 +19,47 @@ public class MainActivityOnCreateAdapter extends MethodVisitor {
     }
 
     @Override
+    public void visitCode() {
+        super.visitCode();
+        System.out.println("onCreate start");
+    }
+
+    @Override
     public void visitInsn(int opcode) {
-        super.visitInsn(opcode);
+
+        // 我要在方法尾部插入代码，我自己return
+        if (opcode == Opcodes.RETURN) {
+            System.out.println("方法执行到 return");
+        } else {
+            super.visitInsn(opcode);
+        }
+    }
+
+    @Override
+    public void visitMaxs(int maxStack, int maxLocals) {
+        // 在代码尾部插入个方法
+        mv.visitLdcInsn("xx");
+        mv.visitLdcInsn("\u6267\u884c\u65b9\u6cd5\u7ed3\u675f\u63d2\u5165\u4ee3\u7801");
+        mv.visitMethodInsn(Opcodes.INVOKESTATIC, "android/util/Log", "d", "(Ljava/lang/String;Ljava/lang/String;)I",
+            false);
+        mv.visitInsn(Opcodes.POP);
+
+        // 自己塞入个return
+        mv.visitInsn(Opcodes.RETURN);
+
+        // 保证最大栈够
+        int returnMaxStack = maxStack;
+        if (returnMaxStack < 2) {
+            returnMaxStack = 2;
+        }
+
+        super.visitMaxs(returnMaxStack, maxLocals);
+        System.out.println("maxStack：" + maxStack + "maxLocals:" + maxLocals);
+    }
+
+    @Override
+    public void visitEnd() {
+        super.visitEnd();
+        System.out.println("onCreate end");
     }
 }
